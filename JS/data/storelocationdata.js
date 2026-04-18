05103c134091080646adb870ebb77037e0b7b4c3
SirConnery <jaakkovallinkoski@gmail.com>
2026-04-18 13:21:25 +0300

    Mobile adjustments and other adjustments.

--- JS/data/storelocationdata.js
+++ JS/data/storelocationdata.js
@@ -3,7 +3,7 @@ const storeLocations = [
     name: "FixIT Mobile Espoo",
     location: "Alberganesplanadi 2, 02600 Espoo (Leppävaara)",
     info: "Quick diagnostics and same-day repairs. Open Mon–Sat 10:00–18:00.",
-    mapImg: "img/contact/map-espoo-alberganesplanadi.webp",
+    mapImg: "./img/contact/map-espoo-alberganesplanadi.webp",
   },
   {
     name: "FixIT Mobile Helsinki",
@@ -15,18 +15,18 @@ const storeLocations = [
     name: "FixIT Mobile Tampere",
     location: "Hämeenkatu 10, 33100 Tampere (City Center)",
     info: "All major brands supported. Warranty included on repairs.",
-    mapImg: "img/contact/map-tampere-hameenkatu.webp",
+    mapImg: "./img/contact/map-tampere-hameenkatu.webp",
   },
   {
     name: "FixIT Mobile Vantaa",
     location: "Ratatie 11, 01300 Vantaa (Tikkurila)",
     info: "Fast phone and tablet repairs. Walk-ins welcome. Open Mon–Fri 10:00–18:00.",
-    mapImg: "img/contact/map-vantaa-ratatie.webp",
+    mapImg: "./img/contact/map-vantaa-ratatie.webp",
   },
   {
     name: "FixIT Mobile Turku",
     location: "Joukahaisenkatu 6, 20520 Turku (Kupittaa)",
     info: "Affordable repairs and accessories available in-store.",
-    mapImg: "img/contact/map-turku-joukahaisenkatu.webp",
+    mapImg: "./img/contact/map-turku-joukahaisenkatu.webp",
   },
 ];

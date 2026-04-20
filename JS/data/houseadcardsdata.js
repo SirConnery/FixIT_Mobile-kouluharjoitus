1dc4fc46cddd5b604b1a44b36a4dfb2fd150bfcf
Daniel S <daniel.sandberg@tuni.fi>
2026-04-20 17:19:37 +0300

    Added Home page fake ad cards images

--- JS/data/houseadcardsdata.js
+++ JS/data/houseadcardsdata.js
@@ -2,7 +2,7 @@ const houseAdsData = [
   {
     headerText: "Nopeat näytön korjaukset",
     text: "Useimmat näytönvaihdot valmistuvat alle tunnissa. Tuo laitteesi meille!",
-    imgSource: "https://placehold.co/300x300",
+    imgSource: "Img/icons/screen_repair.webp",
     imgOnLeftSide: true,
     containerColor: "#FDFCF0",
     cardColor: "#065a9242",
@@ -10,7 +10,7 @@ const houseAdsData = [
   {
     headerText: "Akun kuntotarkastus ja vaihto",
     text: "Tyhjeneekö puhelimesi akku liian nopeasti? Vaihda laadukas akku jo alkaen 39 €.",
-    imgSource: "https://placehold.co/300x300",
+    imgSource: "Img/icons/battery.webp",
     imgOnLeftSide: false,
     containerColor: "#fc950f33",
     cardColor: "#fc950f2f",
@@ -18,7 +18,7 @@ const houseAdsData = [
   {
     headerText: "Vaihda vanha laitteesi",
     text: "Muuta vanha tai rikkinäinen puhelimesi hyvitykseksi. Tuo se meille maksuttomaan arvioon.",
-    imgSource: "https://placehold.co/300x300",
+    imgSource: "Img/icons/trade.webp",
     imgOnLeftSide: true,
     containerColor: "#FDFCF0",
     cardColor: "#065a9242",
